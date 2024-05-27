9933ef8cabef2e0b1affd44ff179fb86e5034678
De-pitcher <emmanwa000@gmail.com>
2024-05-27 11:33:27 +0100

    Added python web

--- weather.py
+++ weather.py
@@ -5,10 +5,7 @@ from pprint import pprint
 
 load_dotenv()
 
-def get_current_weather():
-    print('\n*** Get Current Weather Conditions ***\n')
-    
-    city = input('\nPlease enter a city name:\n')
+def get_current_weather(city="Awka"):
     
     request_url = f'https://api.openweathermap.org/data/2.5/weather?&appid={os.getenv("API_KEY")}&q={city}&units=imperial'
     
@@ -17,9 +14,18 @@ def get_current_weather():
     weather_data = requests.get(request_url).json()
     
     # pprint(weather_data)
-    print(f'\nCurrent weather for {weather_data["name"]}')
-    print(f'\nThe temp is {weather_data["main"]["temp"]}')
-    print(f'\nFeels like {weather_data["main"]["feels_like"]} and {weather_data["weather"][0]["description"]}.')
+    return weather_data
 
 if __name__ == '__main__':
-    get_current_weather()
+    print('\n*** Get Current Weather Conditions ***\n')
+    city = input('\nPlease enter a city name: ')
+    
+    # Check for empty strings or strings with only spaces
+    if not bool(city.strip()):
+        city = 'Awka'
+        
+    weather_data = get_current_weather(city)
+    # print(f'\nCurrent weather for {weather_data["name"]}')
+    # print(f'\nThe temp is {weather_data["main"]["temp"]}')
+    # print(f'\nFeels like {weather_data["main"]["feels_like"]} and {weather_data["weather"][0]["description"]}.')
+    pprint(weather_data)
